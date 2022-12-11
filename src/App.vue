1b23cd5eadd1edf1e1e726fee61cd34628478c2a
unknown <nguyenvietxuan96@gmail.com>
2022-12-11 23:45:30 +0700

    first vuejs frontend project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UsersComp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersComp from './components/UsersComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UsersComp
   }
 }
 </script>
---
+++ src/components/UsersComp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class = "container">
+        <h1 class = "text-center"> Users List </h1>
+        <table class = "table table-striped">
+            <thead>
+                <th> User Id </th>
+                <th> User First Name </th>
+                <th> User Last Name </th>
+                <th> User Email </th>
+            </thead>
+            <tbody>
+                <tr v-for= "user in users" v-bind:key = "user.id">
+                    <td> {{user.id}} </td>
+                    <td> {{user.firstName}} </td>
+                    <td> {{user.lastName}} </td>
+                    <td> {{user.email}} </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+<script>
+import UserService from '../services/UserService';
+    export default {
+        name: 'UsersComp',
+        data(){
+            return {
+               users : [] 
+            }
+        },
+        methods: {
+            getUsers(){
+                UserService.getUsers().then((response) => {
+                    this.users = response.data;
+                });
+            }
+        },
+        created() {
+            this.getUsers();
+        }
+    }
+</script>
+
