46d824e07fcc69e3e416dc97e40b71f2c7e0378d
Xuan. Nguyen Viet - CMC Global DE1 <nvxuan@cmcglobal.vn>
2022-12-16 17:32:02 +0700

    second commit

--- src/components/NewUserComp.vue
+++ src/components/NewUserComp.vue
@@ -1,55 +1,123 @@
 <template>
-    <div>
-        <h4>User Register</h4>
-        <form id="todo-field" v-on:submit.prevent="submitForm" >
-            <span>First Name:</span>
-            <input v-model="users.firstName" placeholder="edit me" />
-            <br><br>
-            <span>Last Name:</span>
-            <input v-model="users.lastName" placeholder="edit me" />
-            <br><br>
-            <span>Email: </span>
-            <input v-model="users.email" placeholder="edit me" />
-            <br><br>
-            <button class = "submit-button" type="submit">Save</button>            
-            <button class = "cancel-button" @click="cancel()">Cancel</button>
-        </form>
-    </div>
-
+  <div>
+    <h4>User Register</h4>
+    <form id="register-form" v-on:submit.prevent="submitForm">
+      <span>First Name:</span>
+      <input
+        type="text"
+        v-model="user.firstName"
+        v-bind:class="{ 'is-valid': errors.firstName }"
+        placeholder="edit me"
+      />
+      <div class="invalid-feedback d-block">{{ errors.firstName }}</div>
+      <br /><br />
+      <span>Last Name:</span>
+      <input
+        type="text"
+        v-model="user.lastName"
+        v-bind:class="{ 'is-valid': errors.firstName }"
+        placeholder="edit me"
+      />
+      <div class="invalid-feedback d-block">{{ errors.lastName }}</div>
+      <br /><br />
+      <span>Email: </span>
+      <input
+        type="text"
+        v-model="user.email"
+        v-bind:class="{ 'is-valid': errors.email }"
+        placeholder="edit me"
+      />
+      <div class="invalid-feedback d-block" v-if="errors.email">
+        {{ errors.email }}
+      </div>
+      <br /><br />
+      <button class="submit-button" type="submit">Save</button>
+      <button class="cancel-button" @click="cancel()">Cancel</button>
+    </form>
+  </div>
 </template>
 <script>
-import UserService from '../services/UserService';
+// import { isVNode } from '@vue/runtime-core/dist/runtime-core';
+import UserService from "../services/UserService";
 export default {
-    data(){
-        return {
-            users : [] 
-        }
-    },
-    methods: {
-        submitForm(){
-            var data = {
-            firstName: this.users.firstName ,
-            lastName: this.users.lastName,
-            email: this.users.email
+  data() {
+    return {
+      errors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+      },
+      user: {},
+      // users : []
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.validate()) {
+        const data = {
+          firstName: this.user.firstName,
+          lastName: this.user.lastName,
+          email: this.user.email,
         };
-            UserService.save(data).then((response) => {
-                console.log(response.data)
-                this.users = response.data;
-                this.$router.push("/");
-            });
-        },
-        cancel(){
+        UserService.save(data)
+          .then((response) => {
+            console.log(" Response data", response.data);
             this.$router.push("/");
-        },
-
+            // this.users = response.data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
-    created() {
-    }
-    
-}
+    // submitForm() {
+    //     this.validate()
+    //     console.log(this.validate())
+    //     console.log(this.errors)
+    //     console.log(this.user)
+    // },
+    cancel() {
+      this.$router.push("/");
+    },
+    validate() {
+      let isValid = true;
+
+      if (!this.user.firstName) {
+        this.errors.firstName = " First Name is required";
+        isValid = false;
+      } else {
+        this.errors.firstName = "";
+      }
+      if (!this.user.lastName) {
+        this.errors.lastName = " Last Name is required";
+        isValid = false;
+      } else {
+        this.errors.lastName = "";
+      }
+      if (!this.user.email) {
+        this.errors.email = " Email is required";
+        isValid = false;
+      } else if (
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.user.email) ==
+        false
+      ) {
+        this.errors.email = "Invalid email format";
+        isValid = false;
+      } else {
+        this.errors.email = "";
+      }
+      return isValid;
+    },
+  },
+  created() {
+    // setInterval(() => {
+    //   console.log(this.user);
+    // }, 2000);
+  },
+};
 </script>
 <style scoped>
-    .submit-button{
-        margin-right: 100px;
-    }
+.submit-button {
+  margin-right: 100px;
+}
 </style>
--- src/components/UserEditComp.vue
+++ src/components/UserEditComp.vue
@@ -1,63 +1,61 @@
 <template>
-    <div>
-        <h4>User Edit</h4>
-        <form id="todo-field" v-on:submit.prevent="submitForm" >
-            <span>First Name:</span>
-            <input v-model="users.firstName" placeholder="edit me" />
-            <br><br>
-            <span>Last Name:</span>
-            <input v-model="users.lastName" placeholder="edit me" />
-            <br><br>
-            <span>Email: </span>
-            <input v-model="users.email" placeholder="edit me" />
-            <br><br>
-            <button class = "submit-button" type="submit">Submit</button>            
-            <button class = "cancel-button" @click="cancel()">Cancel</button>
-        </form>
-    </div>
-
+  <div>
+    <h4>User Edit</h4>
+    <form id="edit-form" v-on:submit.prevent="submitForm">
+      <span>First Name:</span>
+      <input v-model="user.firstName" placeholder="edit me" />
+      <br /><br />
+      <span>Last Name:</span>
+      <input v-model="user.lastName" placeholder="edit me" />
+      <br /><br />
+      <span>Email: </span>
+      <input v-model="user.email" placeholder="edit me" />
+      <br /><br />
+      <button class="submit-button" type="submit">Submit</button>
+      <button class="cancel-button" @click="cancel()">Cancel</button>
+    </form>
+  </div>
 </template>
 <script>
-import UserService from '../services/UserService';
+import UserService from "../services/UserService";
 export default {
-    data(){
-        return {
-            users : [] 
-        }
+  data() {
+    return {
+      user: [],
+    };
+  },
+  methods: {
+    getOne(userId) {
+      UserService.getOne(userId).then((response) => {
+        console.log(response.data);
+        this.user = response.data;
+      });
     },
-    methods: {
-        getOne(userId){
-            UserService.getOne(userId).then((response) => {
-                console.log(response.data)
-                this.users = response.data;
-            });
-        },
-        submitForm(){
-            var data = {
-            id: this.users.id,
-            firstName: this.users.firstName ,
-            lastName: this.users.lastName,
-            email: this.users.email
-        };
-            UserService.update(this.users.id, data).then((response) => {
-                console.log(response.data)
-                this.users = response.data;
-                this.$router.push("/");
-            });
-        },
-        cancel(){
-            this.$router.push("/");
-        },
-
+    submitForm() {
+      var data = {
+        id: this.user.id,
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        email: this.user.email,
+      };
+      UserService.update(this.user.id, data).then((response) => {
+        console.log(response.data);
+        this.user = response.data;
+        this.$router.push("/");
+      });
     },
-    created() {
-        let userId = this.$route.params.id
-        this.getOne(userId);
-    }
-}
+    cancel() {
+      this.$router.push("/");
+    },
+  },
+  created() {
+    let userId = this.$route.params.id;
+    this.getOne(userId);
+  },
+};
 </script>
 <style scoped>
-    .submit-button{
-        margin-right: 100px;
-    }
+.submit-button {
+  margin-right: 100px;
+}
 </style>
